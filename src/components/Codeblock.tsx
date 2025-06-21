2bd0478cbd9dd5eb85d32108294a466dacccbc51
GaviTSRA <gavitsra@gmail.com>
2025-06-21 19:17:15 +0200

    better sidebar

--- src/components/Codeblock.tsx
+++ src/components/Codeblock.tsx
@@ -2,40 +2,41 @@ import { useEffect, useState } from "react";
 import { createHighlighter } from "shiki";
 
 export function CodeBlock({
-  className = "",
-  code,
+	className = "",
+	code,
 }: {
-  className?: string;
-  code: string;
+	className?: string;
+	code: string;
 }) {
-  const [html, setHtml] = useState("");
-  const lang = className.replace(/^language-/, "") || "dfrs";
+	const [html, setHtml] = useState("");
+	const lang = className.replace(/^language-/, "") || "dfrs";
 
-  console.info(code);
+	console.info(code);
 
-  useEffect(() => {
-    async function highlight() {
-      const dfrs = await fetch("/dfrs-docs/dfrs.tmLanguage.json").then((res) =>
-        res.json()
-      );
+	useEffect(() => {
+		async function highlight() {
+			const dfrs = await fetch("/dfrs-docs/dfrs.tmLanguage.json").then((res) =>
+				res.json(),
+			);
 
-      const highlighter = await createHighlighter({
-        themes: ["dark-plus"],
-        langs: [dfrs],
-      });
+			const highlighter = await createHighlighter({
+				themes: ["dark-plus"],
+				langs: [dfrs],
+			});
 
-      const html = highlighter.codeToHtml(code, {
-        lang,
-        theme: "dark-plus",
-        colorReplacements: {
-          "#1e1e1e": "var(--color-surface)",
-        },
-      });
-      setHtml(html);
-    }
+			const html = highlighter.codeToHtml(code, {
+				lang,
+				theme: "dark-plus",
+				colorReplacements: {
+					"#1e1e1e": "var(--color-surface)",
+				},
+			});
+			setHtml(html);
+		}
 
-    highlight();
-  }, [code, lang]);
+		highlight();
+	}, [code, lang]);
 
-  return <div className="w-fit" dangerouslySetInnerHTML={{ __html: html }} />;
+	// biome-ignore lint/security/noDangerouslySetInnerHtml: Required for code shiki
+	return <div className="w-fit" dangerouslySetInnerHTML={{ __html: html }} />;
 }
--- src/pages/Docs.tsx
+++ src/pages/Docs.tsx
@@ -1,111 +1,180 @@
 import { useNavigate, useParams } from "react-router";
-import { lazy, Suspense, useEffect, useMemo, useState } from "react";
+import { lazy, Suspense, useEffect, useState } from "react";
 
 const pages = import.meta.glob("../doc/**/*.mdx");
+import { GoChevronDown, GoChevronUp } from "react-icons/go";
+import { AnimatePresence, motion } from "motion/react";
 
 type Section = {
-  id: string;
-  title: string;
-  children?: Section[];
+	id: string;
+	title: string;
+	children?: Section[];
 };
 
 const sections: Section[] = [
-  {
-    id: "getting-started",
-    title: "Getting Started",
-  },
-  {
-    id: "basics",
-    title: "Basics",
-  },
-  {
-    id: "concepts",
-    title: "Concepts",
-    children: [
-      {
-        id: "variables",
-        title: "Variables",
-      },
-      {
-        id: "functions",
-        title: "Functions",
-      },
-    ],
-  },
+	{
+		id: "getting-started",
+		title: "Getting Started",
+	},
+	{
+		id: "basics",
+		title: "Basics",
+		children: [
+			{
+				id: "events",
+				title: "Events",
+			},
+		],
+	},
+	{
+		id: "concepts",
+		title: "Concepts",
+		children: [
+			{
+				id: "variables",
+				title: "Variables",
+			},
+			{
+				id: "functions",
+				title: "Functions",
+			},
+		],
+	},
 ];
 
 function SectionList({
-  section,
-  pathPrefix,
+	section,
+	pathPrefix,
 }: {
-  section: Section;
-  pathPrefix?: string;
+	section: Section;
+	pathPrefix?: string;
 }) {
-  const navigate = useNavigate();
+	const navigate = useNavigate();
+	const { section: path } = useParams();
+	const [collapsed, setCollapsed] = useState(true);
 
-  return (
-    <div>
-      <p
-        className="text-text hover:text-text-secondary cursor-pointer"
-        onClick={() => {
-          navigate(`/docs/${pathPrefix ? `${pathPrefix}.` : ""}${section.id}`);
-        }}
-        onKeyDown={() => {
-          navigate(`/docs/${pathPrefix ? `${pathPrefix}.` : ""}${section.id}`);
-        }}
-      >
-        {section.title}
-      </p>
-      <div className="ml-4">
-        {section.children?.map((subSection) => (
-          <SectionList
-            key={subSection.id}
-            section={subSection}
-            pathPrefix={pathPrefix ? `${pathPrefix}.${section.id}` : section.id}
-          />
-        ))}
-      </div>
-    </div>
-  );
+	const selected = path === (pathPrefix ? `${pathPrefix}.` : "") + section.id;
+
+	useEffect(() => {
+		if (path === (pathPrefix ? `${pathPrefix}.` : "") + section.id) {
+			setCollapsed(false);
+		}
+		if (section.children) {
+			for (const child of section.children) {
+				if (
+					path ===
+					(pathPrefix ? `${pathPrefix}.${section.id}.` : `${section.id}.`) +
+						child.id
+				) {
+					setCollapsed(false);
+					return;
+				}
+			}
+		}
+	}, [path, pathPrefix, section]);
+
+	return (
+		<div>
+			<motion.div
+				initial={false}
+				className="flex flex-row items-center gap-1 select-none"
+			>
+				<p
+					className={`cursor-pointer text-base transition duration-300 ${selected ? "text-white hover:text-text-title font-semibold" : "text-text-title hover:text-text-secondary"}`}
+					onClick={() => {
+						setCollapsed(false);
+						navigate(
+							`/docs/${pathPrefix ? `${pathPrefix}.` : ""}${section.id}`,
+						);
+					}}
+				>
+					{section.title}
+				</p>
+				{section.children && (
+					<div
+						onClick={() => setCollapsed(!collapsed)}
+						className="ml-auto cursor-pointer"
+					>
+						{collapsed && <GoChevronDown />}
+						{!collapsed && <GoChevronUp />}
+					</div>
+				)}
+			</motion.div>
+
+			<AnimatePresence initial={false}>
+				{!collapsed &&
+					section.children &&
+					section.children.map((subSection, index) => {
+						const selected =
+							path ===
+							(pathPrefix ? `${pathPrefix}.${section.id}.` : `${section.id}.`) +
+								subSection.id;
+						return (
+							<motion.div
+								initial="collapsed"
+								animate="open"
+								exit="collapsed"
+								variants={{
+									open: { opacity: 1, height: "auto" },
+									collapsed: { opacity: 0, height: 0 },
+								}}
+								transition={{
+									delay: index * 0.05,
+								}}
+								className={`pl-4 transition duration-300 ${selected ? "border-primary border-l-2" : "border-border-hover border-l-1"}`}
+								key={subSection.id}
+							>
+								<SectionList
+									section={subSection}
+									pathPrefix={
+										pathPrefix ? `${pathPrefix}.${section.id}` : section.id
+									}
+								/>
+							</motion.div>
+						);
+					})}
+			</AnimatePresence>
+		</div>
+	);
 }
 
 export function Docs() {
-  const { section } = useParams();
-  const [Component, setComponent] = useState<React.LazyExoticComponent<
-    React.ComponentType<any>
-  > | null>(null);
+	const { section } = useParams();
+	const [Component, setComponent] = useState<React.LazyExoticComponent<
+		React.ComponentType<any>
+	> | null>(null);
 
-  useEffect(() => {
-    if (!section) {
-      setComponent(null);
-      return;
-    }
+	useEffect(() => {
+		if (!section) {
+			setComponent(null);
+			return;
+		}
 
-    const basePath = `../doc/${section.replace(/\./g, "/")}`;
-    const fullPath = pages[`${basePath}.mdx`]
-      ? `${basePath}.mdx`
-      : `${basePath}/index.mdx`;
-    const loader = pages[fullPath];
+		const basePath = `../doc/${section.replace(/\./g, "/")}`;
+		const fullPath = pages[`${basePath}.mdx`]
+			? `${basePath}.mdx`
+			: `${basePath}/index.mdx`;
+		const loader = pages[fullPath];
 
-    if (loader) {
-      setComponent(() => lazy(loader));
-    } else {
-      setComponent(null);
-    }
-  }, [section]);
+		if (loader) {
+			setComponent(() => lazy(loader));
+		} else {
+			setComponent(null);
+		}
+	}, [section]);
 
-  return (
-    <div className="grid grid-cols-[1fr_5fr] h-full gap-4">
-      <div className="bg-surface border-border border-r-1 h-full p-2 text-lg">
-        {sections.map((section) => (
-          <SectionList key={section.id} section={section} />
-        ))}
-      </div>
-      <div>
-        <Suspense fallback={<p>Loading...</p>}>
-          {Component ? <Component /> : <p>Not found</p>}
-        </Suspense>
-      </div>
-    </div>
-  );
+	return (
+		<div className="grid grid-cols-[1fr_6fr] h-full gap-4">
+			<div className="bg-surface h-full p-2 text-lg border-r-1 border-border">
+				{sections.map((section) => (
+					<SectionList key={section.id} section={section} />
+				))}
+			</div>
+			<div>
+				<Suspense fallback={<p>Loading...</p>}>
+					{Component ? <Component /> : <p>Not found</p>}
+				</Suspense>
+			</div>
+		</div>
+	);
 }
